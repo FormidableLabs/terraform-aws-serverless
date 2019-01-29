f5db55be5d42a74f8a46a4e17890aee7df2c0fc4
Ryan Roemer <ryan.roemer@formidable.com>
2019-01-29 13:34:11 -0800

    Add starting groups

---
+++ modules/iam/main.tf
@@ -0,0 +1,23 @@
+###############################################################################
+# IAM Groups
+#
+# - `admin`: An administrator that can create, delete, develop the services.
+# - `developer`: A developer that deploy an existing service.
+# - `ci`: The CI service can deploy an existing service.
+#
+# General reference
+# - https://iam.cloudonaut.io/
+# - https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
+# - http://awspolicygen.s3.amazonaws.com/policygen.html
+###############################################################################
+resource "aws_iam_group" "admin" {
+  name = "${local.tf_service_name}-${var.environment}-admin"
+}
+
+resource "aws_iam_group" "developer" {
+  name = "${local.tf_service_name}-${var.environment}-developer"
+}
+
+resource "aws_iam_group" "ci" {
+  name = "${local.tf_service_name}-${var.environment}-ci"
+}

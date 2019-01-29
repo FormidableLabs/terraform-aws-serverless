e5f767d97c5d97d8bdeb3a4197449069b48b2ee1
Ryan Roemer <ryan.roemer@formidable.com>
2019-01-29 12:20:48 -0800

    Lots of temp refactoring.

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 1.19"
-}
+# TODO: MOVE THIS TOO?
+# provider "aws" {
+#   region  = "${var.region}"
+#   version = "~> 1.19"
+# }
 
 # TODO: Move all of this to integrating app and document?
-terraform {
-  backend "s3" {
-    # TODO: Choose a better name?
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     # TODO: Choose a better name?
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
---
+++ modules/iam/variables.tf
@@ -0,0 +1,36 @@
+# TODO: Move these variables up to root?
+
+# AWS
+variable "region" {
+  description = "The deploy target region in AWS"
+  default     = "us-east-1"
+}
+
+variable "iam_region" {
+  description = "The IAM region restriction for permissions (defaults to 'any region')."
+  default     = "*"
+}
+
+# Our custom stack / environment
+variable "environment" {
+  description = "The stage/environment to deploy to. Suggest: `sandbox`, `development`, `staging`, `production`."
+  default     = "development"
+}
+
+variable "service_name" {
+  description = "Name of service / application"
+}
+
+# Terraform service name.
+# _Note_: Defaults are performed in local variables.
+variable "tf_service_name" {
+  description = "The unique name of service for Terraform resources. Defaults to: `tf-SERVICE_NAME`."
+  default     = ""
+}
+
+# Serverless version (to synchronize with).
+# _Note_: Defaults are performed in local variables.
+variable "sls_service_name" {
+  description = "The service name from Serverless configuration. Defaults to: `sls-SERVICE_NAME`."
+  default     = ""
+}

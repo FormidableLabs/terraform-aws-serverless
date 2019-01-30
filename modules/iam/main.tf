209fb74f8dd4665c65587fa24d0c9f970ebfdf14
Ryan Roemer <ryan.roemer@formidable.com>
2019-01-29 16:07:52 -0800

    Add starting groups

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -10,10 +10,6 @@
 # - https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
 # - http://awspolicygen.s3.amazonaws.com/policygen.html
 ###############################################################################
-resource "aws_iam_group" "admin" {
-  name = "${local.tf_service_name}-${var.stage}-admin"
-}
-
 resource "aws_iam_group" "developer" {
   name = "${local.tf_service_name}-${var.stage}-developer"
 }

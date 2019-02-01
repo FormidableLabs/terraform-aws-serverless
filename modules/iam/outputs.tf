9e24631f8d92f6c751f2cae1d0face80f02fc644
Ryan Roemer <ryan.roemer@formidable.com>
2019-02-01 14:46:29 -0800

    WIP: Start outputs.

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,1 +1,21 @@
+# Outputs
 
+output "admin_group_name" {
+  value = "${aws_iam_group.admin.name}"
+}
+
+output "ci_group_name" {
+  value = "${aws_iam_group.ci.name}"
+}
+
+output "developer_group_name" {
+  value = "${aws_iam_group.developer.name}"
+}
+
+output "lambda_execution_role_name" {
+  value = "${aws_iam_role.lambda_execution.name}"
+}
+
+output "lambda_execution_role_arn" {
+  value = "${aws_iam_role.lambda_execution.arn}"
+}

ee84e3ec778021f5b705095b5b9fd2366f41c342
Ryan Roemer <ryan.roemer@formidable.com>
2019-02-08 10:53:25 -0800

    WIP: Initial experiment

--- modules/iam/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Outputs
-
-output "lambda_execution_role_arn" {
-  value = "${aws_iam_role.lambda_execution.arn}"
-}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,5 @@
+# Outputs
 
+output "lambda_execution_role_arn" {
+  value = "${aws_iam_role.lambda_execution.arn}"
+}

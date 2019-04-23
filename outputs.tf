f48a2b692afaab0676388d615f6e89c760ba905b
Ryan Roemer <ryan.roemer@formidable.com>
2019-04-22 20:59:56 -0700

    Add outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,13 @@
 # Outputs
 
+output "iam_group_admin_name" {
+  value = "${aws_iam_group.admin.name}"
+}
+
+output "iam_group_developer_name" {
+  value = "${aws_iam_group.developer.name}"
+}
+
+output "iam_group_ci_name" {
+  value = "${aws_iam_group.ci.name}"
+}

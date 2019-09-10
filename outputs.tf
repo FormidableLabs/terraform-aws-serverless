7468ab80d6b6f59161d8e1324494a283523585a3
Tyler Thompson <tyler.thompson@formidable.com>
2019-09-10 09:43:40 -0700

    Fix conditional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,22 @@
 # Outputs
 
+# Use the dummy list trick to fix conditional outputs.
+# This is only an issue in Terraform versions earlier than 0.12.x.
+# See https://github.com/hashicorp/terraform/issues/9858#issuecomment-386431631
+locals {
+  empty_list = [""]
+}
+
 output "iam_group_admin_name" {
-  value = "${aws_iam_group.admin.name}"
+  value = "${element(concat(local.empty_list, aws_iam_group.admin.*.name), 1)}"
 }
 
 output "iam_group_developer_name" {
-  value = "${aws_iam_group.developer.name}"
+  value = "${element(concat(local.empty_list, aws_iam_group.developer.*.name), 1)}"
 }
 
 output "iam_group_ci_name" {
-  value = "${aws_iam_group.ci.name}"
+  value = "${element(concat(local.empty_list, aws_iam_group.ci.*.name), 1)}"
 }
 
 output "iam_policy_admin_arn" {
@@ -29,9 +36,9 @@ output "iam_policy_cd_lambdas_arn" {
 }
 
 output "lambda_role_arn" {
-  value = "${aws_iam_role.lambda.*.arn[0]}"
+  value = "${element(concat(local.empty_list, aws_iam_role.lambda.*.arn), 1)}"
 }
 
 output "lambda_role_name" {
-  value = "${aws_iam_role.lambda.*.name[0]}"
+  value = "${element(concat(local.empty_list, aws_iam_role.lambda.*.name), 1)}"
 }

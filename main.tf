94d521c8e6b3c7c3b74a30026d4960bf8380f1d4
Tyler Thompson <tyler.thompson@formidable.com>
2019-09-17 15:36:02 -0700

    Use explicit count.index for Terraform 0.12

--- main.tf
+++ main.tf
@@ -19,19 +19,19 @@ resource "aws_iam_group" "admin" {
 
 resource "aws_iam_group_policy_attachment" "admin_admin" {
   count      = "${local.opt_disable_groups ? 0 : 1}"
-  group      = "${aws_iam_group.admin.name}"
+  group      = "${element(aws_iam_group.admin.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.admin.arn}"
 }
 
 resource "aws_iam_group_policy_attachment" "admin_cd_lambdas" {
   count      = "${local.opt_disable_groups ? 0 : 1}"
-  group      = "${aws_iam_group.admin.name}"
+  group      = "${element(aws_iam_group.admin.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.cd_lambdas.arn}"
 }
 
 resource "aws_iam_group_policy_attachment" "admin_developer" {
   count      = "${local.opt_disable_groups ? 0 : 1}"
-  group      = "${aws_iam_group.admin.name}"
+  group      = "${element(aws_iam_group.admin.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.developer.arn}"
 }
 
@@ -43,13 +43,13 @@ resource "aws_iam_group" "ci" {
 
 resource "aws_iam_group_policy_attachment" "ci_developer" {
   count      = "${local.opt_disable_groups ? 0 : 1}"
-  group      = "${aws_iam_group.ci.name}"
+  group      = "${element(aws_iam_group.ci.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.developer.arn}"
 }
 
 resource "aws_iam_group_policy_attachment" "ci_cd_lambdas" {
   count      = "${!local.opt_disable_groups && local.opt_many_lambdas ? 1 : 0}"
-  group      = "${aws_iam_group.ci.name}"
+  group      = "${element(aws_iam_group.ci.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.cd_lambdas.arn}"
 }
 
@@ -61,12 +61,12 @@ resource "aws_iam_group" "developer" {
 
 resource "aws_iam_group_policy_attachment" "developer_developer" {
   count      = "${local.opt_disable_groups ? 0 : 1}"
-  group      = "${aws_iam_group.developer.name}"
+  group      = "${element(aws_iam_group.developer.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.developer.arn}"
 }
 
 resource "aws_iam_group_policy_attachment" "developer_cd_lambdas" {
   count      = "${!local.opt_disable_groups && local.opt_many_lambdas ? 1 : 0}"
-  group      = "${aws_iam_group.developer.name}"
+  group      = "${element(aws_iam_group.developer.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.cd_lambdas.arn}"
 }
